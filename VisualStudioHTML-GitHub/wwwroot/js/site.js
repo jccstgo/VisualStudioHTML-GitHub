6f5c31f5e7a6bf7cac5a5f5f720806f767204d2d
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-11-24 11:56:39 -0800

    Add project files.

---
+++ VisualStudioHTML-GitHub/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var inc = 1000;
+
+clock();
+
+function clock() {
+    const date = new Date();
+
+    const hours = ((date.getHours() + 11) % 12 + 1);
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+
+    document.querySelector('.hour').style.transform = `rotate(${hour}deg)`
+    document.querySelector('.minute').style.transform = `rotate(${minute}deg)`
+    document.querySelector('.second').style.transform = `rotate(${second}deg)`
+}
+
+setInterval(clock, inc);
